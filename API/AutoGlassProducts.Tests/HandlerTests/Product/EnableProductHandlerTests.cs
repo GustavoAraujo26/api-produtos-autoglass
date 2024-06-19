d4e3b368de619febe451ad0e48c74ebcf068ecc3
Gustavo de Araujo Oliveira <gustavo.dearaujo26@gmail.com>
2024-06-19 20:30:34 -0300

    Unit tests for EnableProductHandler

---
+++ API/AutoGlassProducts.Tests/HandlerTests/Product/EnableProductHandlerTests.cs
@@ -0,0 +1,127 @@
+﻿using AutoGlassProducts.Domain.Handlers.Product;
+using AutoGlassProducts.Domain.Repositories;
+using AutoGlassProducts.Tests.FakeData.Product.Entity;
+using AutoGlassProducts.Tests.FakeData.Product.Request;
+using AutoGlassProducts.Tests.FakeData.Product.Response;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AutoGlassProducts.Tests.HandlerTests.Product
+{
+    public class EnableProductHandlerTests
+    {
+        private readonly Mock<IProductRepository> _productRepositoryMock;
+        private readonly CancellationTokenSource _cts;
+
+        public EnableProductHandlerTests()
+        {
+            _productRepositoryMock = new Mock<IProductRepository>();
+            _cts = new CancellationTokenSource();
+        }
+
+        [Fact]
+        public async Task WhenValidRequest_ReturnSuccess()
+        {
+            //Arrange
+            var serviceCollection = TestEnvironment.BuildEnvironment();
+
+            _productRepositoryMock.Setup(x => x.Save(It.IsAny<Domain.Entities.Product>()))
+                .ReturnsAsync(ProductResponseFakeData.Build());
+
+            _productRepositoryMock.Setup(x => x.Get(It.IsAny<int>()))
+                .ReturnsAsync(ProductFakeData.Build());
+
+            serviceCollection.AddTransient(x => _productRepositoryMock.Object);
+
+            var request = EnableProductRequestFakeData.BuildValid();
+
+            var handler = serviceCollection.GetService<IEnableProductHandler>();
+
+            //Act
+            var response = await handler.Handle(request, _cts.Token);
+
+            //Assert
+            Assert.True(response.IsSuccess);
+            Assert.NotNull(response.Content);
+        }
+
+        [Fact]
+        public async Task WhenNullRequest_ReturnFailure()
+        {
+            //Arrange
+            var serviceCollection = TestEnvironment.BuildEnvironment();
+
+            _productRepositoryMock.Setup(x => x.Save(It.IsAny<Domain.Entities.Product>()))
+                .ReturnsAsync(ProductResponseFakeData.Build());
+
+            _productRepositoryMock.Setup(x => x.Get(It.IsAny<int>()))
+                .ReturnsAsync(ProductFakeData.Build());
+
+            serviceCollection.AddTransient(x => _productRepositoryMock.Object);
+
+            var handler = serviceCollection.GetService<IEnableProductHandler>();
+
+            //Act
+            var response = await handler.Handle(null, _cts.Token);
+
+            //Assert
+            Assert.True(response.IsFailure);
+            Assert.Null(response.Content);
+        }
+
+        [Fact]
+        public async Task WhenInvalidRequest_ReturnFailure()
+        {
+            //Arrange
+            var serviceCollection = TestEnvironment.BuildEnvironment();
+
+            _productRepositoryMock.Setup(x => x.Save(It.IsAny<Domain.Entities.Product>()))
+                .ReturnsAsync(ProductResponseFakeData.Build());
+
+            _productRepositoryMock.Setup(x => x.Get(It.IsAny<int>()))
+                .ReturnsAsync(ProductFakeData.Build());
+
+            serviceCollection.AddTransient(x => _productRepositoryMock.Object);
+
+            var request = EnableProductRequestFakeData.BuildInvalid();
+
+            var handler = serviceCollection.GetService<IEnableProductHandler>();
+
+            //Act
+            var response = await handler.Handle(request, _cts.Token);
+
+            //Assert
+            Assert.True(response.IsFailure);
+            Assert.Null(response.Content);
+        }
+
+        [Fact]
+        public async Task WhenProductNotFound_ReturnFailure()
+        {
+            //Arrange
+            var serviceCollection = TestEnvironment.BuildEnvironment();
+
+            _productRepositoryMock.Setup(x => x.Save(It.IsAny<Domain.Entities.Product>()))
+                .ReturnsAsync(ProductResponseFakeData.Build());
+
+            _productRepositoryMock.Setup(x => x.Get(It.IsAny<int>()))
+                .ReturnsAsync(default(Domain.Entities.Product));
+
+            serviceCollection.AddTransient(x => _productRepositoryMock.Object);
+
+            var request = EnableProductRequestFakeData.BuildValid();
+
+            var handler = serviceCollection.GetService<IEnableProductHandler>();
+
+            //Act
+            var response = await handler.Handle(request, _cts.Token);
+
+            //Assert
+            Assert.True(response.IsFailure);
+            Assert.Null(response.Content);
+        }
+    }
+}
